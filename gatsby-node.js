a900d55732638c324c4e78efcc16fd354829b970
Bernhard, Dave(AWF) <dbernhard@ebay.com>
2020-10-23 09:10:12 +0100

    feat: create blog post pages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,48 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createFilePath } = require("gatsby-source-filesystem")
+const path = require(`path`)
+
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions
+    const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.tsx")
+
+    return graphql(`
+        {
+            allMdx {
+                nodes {
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+        }
+    `).then(result => {
+        if (result.errors) {
+            throw result.errors
+        }
+
+        const posts = result.data.allMdx.nodes
+
+        // create a page for each mdx file
+        posts.forEach(post => {
+            createPage({
+                path: post.fields.slug,
+                component: blogPostTemplate,
+                context: {
+                    slug: post.fields.slug
+                }
+            })
+        })
+    })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
     const { createNodeField } = actions
